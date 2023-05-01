3acc542a06962163d023a4b3f29677c4378c3604
Fabio Vergani <fabio.vergani.1975@gmail.com>
2023-05-01 16:28:08 +0200

    Update getCommonPrefix.mjs

--- getCommonPrefix.mjs
+++ getCommonPrefix.mjs
@@ -1,40 +1,38 @@
 /**
  * Returns the common prefix of two strings.
- * @param {string} a
- * @param {string} b
- * @returns {string}
+ *
+ * @param {string} a - The first string to compare.
+ * @param {string} b - The second string to compare.
+ * @returns {string} (an empty string if there is no common prefix)
  */
 // prettier-ignore
 const getCommonPrefix = (a, b) => {
 	let prefix = '';
 	if (a && b) {
-		let left = Math.min(a.length, b.length);
-		let right = 0;
-		while (right < left) {
-			const mid = ((right + left) / 2) | 0;
-			const n = 1 + mid;
-			if (a.slice(0, n) === b.slice(0, n)) {
-				right = n;
+		let r = 0, l = Math.min(a.length, b.length);
+		while (r < l) {
+			const j = ((r + l) / 2) | 0, n = 1 + j;
+			if (a.slice(0, n) !== b.slice(0, n)) {
+				l = j;
 			} else {
-				left = mid;
+				r = n;
 			}
 		}
-		const str = a.slice(0, right);
-		const len = str.length;
-		if (len) {
+		const s = a.slice(0, r), n = s.length;
+		if (n) {
 			let i = 0;
-			while (i < len) {
-				const c1 = str[i];
-				const c2 = b[i];
-				if (c1 !== c2 || str.codePointAt(i) !== b.codePointAt(i)) {
+			while (i < n) {
+				const c = s[i], d = b[i];
+				if (c !== d || s.codePointAt(i) !== b.codePointAt(i)) {
 					break;
 				}
 				++i;
 			}
-			prefix = str.slice(0, i);
+			prefix = s.slice(0, i);
 		}
 	}
 	return prefix;
 };
+
 
 export default getCommonPrefix;
